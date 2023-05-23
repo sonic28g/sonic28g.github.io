8fe54a0bf90d5cc1a88362cded7e843276d23ecf
GuilhermeSantos <gui28ss@gmail.com>
2023-05-23 10:45:22 +0100

    v0.1.1
    Error on js typewrite() to fix

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-setTimeout()
+var i = 0;
+var txt = "return \"Welcome!\""; 
+var speed = 50; 
+
+function typeWriter() {
+
+    var bar = 0;
+    while(bar < 6){
+        if(bar % 2 == 0){
+            document.getElementById("ret").innerHTML = "|"
+        }else{
+            document.getElementById("ret").innerHTML = " "
+        }
+        setTimeout(typeWriter, speed*2);
+    }
+
+    if (i < txt.length) {
+        document.getElementById("ret").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
